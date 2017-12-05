2d5e1678b67aad4ae18387cacba07bc5e6f9039e
Anondo <aanondos@gmail.com>
2017-12-05 09:43:01 +0600

    added unique email address feature for signup

--- Views/action/email_exists.php
+++ Views/action/email_exists.php
@@ -4,7 +4,6 @@ require(get_include_path()."\Projects\aiub project\Controllers\SignupController.
 $signup = new SignupController();
 $email = $_GET["email"];
 
-
 $ok = $signup->emailExists($email);
 if($ok)
 {
